06ac7ac725984dfbd8a42d6ec9081d114e223523
kaleido <kaleido@gmail.com>
2019-04-28 08:28:27 +0900

    import styled-component

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import "./App.css";
+import styled, { css } from "styled-components";
+import WelcomeMessage from "./WelcomeMessage";
 
 class App extends Component {
 	render() {
-		return <div>app</div>;
+		return (
+			<div>
+				<WelcomeMessage />
+			</div>
+		);
 	}
 }
 export default App;
---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WelcomeMessage = () => {
+	return <div>hello</div>;
+};
+
+export default WelcomeMessage;
