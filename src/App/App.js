30b5234e524f9806610bafac7484f55f635feffb
kaleido <kaleido@gmail.com>
2019-04-28 09:01:51 +0900

    Add AppbBar and grid

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import styled, { css } from "styled-components";
 import WelcomeMessage from "./WelcomeMessage";
 import AppLayout from "./AppLayout";
+import AppBar from "./AppBar";
 
 class App extends Component {
 	render() {
 		return (
 			<AppLayout>
+				<AppBar />
 				<WelcomeMessage />
 			</AppLayout>
 		);
