c20a0c64fd35b1c04829c277b81ddc2867369084
kaleido <kaleido@gmail.com>
2019-04-29 00:20:35 +0900

    finish Add/Remove coins

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import cc from "cryptocompare";
+import _ from "lodash";
 
 export const AppContext = React.createContext();
+const MAX_FAVORITE = 10;
 
 export class AppProvider extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			page: "dashboard",
+			favorites: ["BTC", "ETC", "XMR", "DOGE"],
 			...this.savedSettings(),
 			setPage: this.setPage,
+			addCoin: this.addCoin,
+			removeCoin: this.removeCoin,
+			isInFavorites: this.isInFavorites,
 			confirmFavorites: this.confirmFavorites
 		};
 	}
@@ -17,6 +23,21 @@ export class AppProvider extends Component {
 	componentDidMount = () => {
 		this.fetchCoins();
 	};
+
+	addCoin = key => {
+		let favorites = [...this.state.favorites];
+		if (favorites.length < MAX_FAVORITE) {
+			favorites.push(key);
+			this.setState({ favorites });
+		}
+	};
+
+	removeCoin = key => {
+		let favorites = [...this.state.favorites];
+		this.setState({ favorites: _.pull(favorites, key) });
+	};
+
+	isInFavorites = key => _.includes(this.state.favorites, key);
 
 	fetchCoins = async () => {
 		let coinList = (await cc.coinList()).Data;
@@ -32,7 +53,7 @@ export class AppProvider extends Component {
 		localStorage.setItem(
 			"cryptoDash",
 			JSON.stringify({
-				test: "hello"
+				favorites: this.state.favorites
 			})
 		);
 	};
@@ -42,8 +63,11 @@ export class AppProvider extends Component {
 		if (!cryptoDashData) {
 			return { page: "settings", firstVisit: true };
 		}
-		return {};
+		console.log(cryptoDashData);
+		let { favorites } = cryptoDashData;
+		return { favorites };
 	};
+
 	setPage = page => {
 		this.setState({ page });
 	};
--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -4,20 +4,40 @@ import { SelectableTile, DeletableTile, DisabledTile } from "../Shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
 import CoinImage from "./CoinImage";
 
+const clickCoinHandler = (topSection, coinKey, addCoin, removeCoin) => {
+	return topSection
+		? () => {
+				removeCoin(coinKey);
+		  }
+		: () => {
+				addCoin(coinKey);
+		  };
+};
+
 export default function CoinTile({ coinKey, topSection }) {
 	return (
 		<AppContext.Consumer>
-			{({ coinList }) => {
+			{({ coinList, addCoin, removeCoin, isInFavorites }) => {
 				let coin = coinList[coinKey];
 
-				const TileClass = topSection ? DeletableTile : SelectableTile;
+				const TileClass = topSection
+					? DeletableTile
+					: isInFavorites(coinKey)
+					? DisabledTile
+					: SelectableTile;
 
 				// const TileClass = SelectableTile;
 				// if(topSection)={
 				//   TileClass=DeletableTile
 				// }
 				return (
-					<TileClass>
+					<TileClass
+						onClick={clickCoinHandler(
+							topSection,
+							coinKey,
+							addCoin,
+							removeCoin
+						)}>
 						<CoinHeaderGrid
 							topSection={topSection}
 							name={coin.CoinName}
