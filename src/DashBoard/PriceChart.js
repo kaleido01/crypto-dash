149ef165bfb5a50704763d856590b9600cdc7bd1
kaleido <kaleido@gmail.com>
2019-04-30 22:07:42 +0900

    HighCharts theme

--- src/DashBoard/PriceChart.js
+++ src/DashBoard/PriceChart.js
@@ -3,6 +3,9 @@ import HighchartsConfig from "./HighchartsConfig";
 import { Tile } from "../Shared/Tile";
 import { AppContext } from "../App/AppProvider";
 import ReactHighCharts from "react-highcharts";
+import HighchartsTheme from "./HighchartsTheme";
+
+ReactHighCharts.Highcharts.setOptions(HighchartsTheme);
 
 export default function PriceChart() {
 	return (
